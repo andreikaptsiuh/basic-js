f9fad4a80422ff72618e0e988e3f2113cf0b9256
Andrei <76453952+andreikaptsiuh@users.noreply.github.com>
2021-01-22 16:55:47 +0300

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr)) return error
+  let result =[];
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] == `--double-next` && i+1 < arr.length){
+      result.push(arr[i+1])
+      continue
+    } 
+    if(arr[i] == `--double-prev` && i > 0 && arr[i-2] != '--discard-next'){
+      result.push(arr[i-1])
+      continue
+    }
+    result.push(arr[i])
+  }
+  const discardCommands = [`--discard-next`,  `--discard-prev`, `--double-next`, `--double-prev`];
+  return result.filter((item, index) => !discardCommands.includes(item) &&  arr[index - 1] != `--discard-next` &&
+  arr[index+1] != `--discard-prev`)
 };
